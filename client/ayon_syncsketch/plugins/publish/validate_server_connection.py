947d05c22b2a8eb1f586bc741931da02f7e13bf3
Jakub Jezek <jakubjezek001@gmail.com>
2023-06-06 14:21:52 +0200

    update validator content

--- client/ayon_syncsketch/plugins/publish/validate_server_connection.py
+++ client/ayon_syncsketch/plugins/publish/validate_server_connection.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Validate a SyncSketch connection."""
 import pyblish.api
+from ayon_syncsketch.api.server_handler import ServerCommunication
+import requests
 
 
 class ValidateServerConnection(pyblish.api.ContextPlugin):
@@ -10,7 +12,7 @@ class ValidateServerConnection(pyblish.api.ContextPlugin):
     syncsketch_id is required to be set on context.data.
     """
 
-    order = pyblish.api.CollectorOrder
+    order = pyblish.api.ValidatorOrder
     label = "Validate SyncSketch Connection"
 
     def process(self, context):
@@ -26,3 +28,12 @@ class ValidateServerConnection(pyblish.api.ContextPlugin):
         self.log.info("Validating SyncSketch connection...")
         self.log.info("SyncSketch ID: {}".format(syncsketch_id))
         self.log.info("SyncSketch server config: {}".format(server_config))
+
+        response = self.get_json(server_config.get("url"))
+
+        context.data["syncsketchServerResponse"] = response
+
+    def get_json(self, url):
+        resp = requests.get(url)
+        assert resp.status_code == 200
+        return resp.json()
