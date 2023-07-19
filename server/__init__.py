9c36070c9d89f814f2066eccc19174a0c0995fd4
Oscar Domingo <oscardomingo@gmail.com>
2023-07-19 14:13:57 +0100

    Updated logic to query and create Webhooks on SyncSketch

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,22 @@
 import json
-from typing import Type
+from typing import Type, Any
 
 from ayon_server.addons import BaseServerAddon, AddonLibrary
 from ayon_server.config import ayonconfig
 from ayon_server.lib.postgres import Postgres
+from ayon_server.types import OPModel
 
 from .settings import SyncsketchSettings, DEFAULT_VALUES
 
+from nxtools import logging
 import requests
+
+
+class SyncsketchRequestModel(OPModel):
+    """ TODO: Once we get the corerct acces for the SyncSketch API implement a proper
+        model to handle the webhook POST paylaod.
+    """
+    payload: dict[str, Any]
 
 
 class SyncsketchAddon(BaseServerAddon):
@@ -21,17 +30,18 @@ class SyncsketchAddon(BaseServerAddon):
         return settings_model_cls(**DEFAULT_VALUES)
 
     async def setup(self):
-        self.create_syncsketch_webhook()
         need_restart = await self.create_applications_attribute()
         if need_restart:
             self.request_server_restart()
+
+        await self.create_syncsketch_webhook()
 
     def initialize(self):
         logging.info("Initializing SyncSketch Addon.")
 
         self.add_endpoint(
             "syncsketch-event",
-            self._receive_syncsketch_event,
+            self._syncsketch_event,
             method="POST",
             name="receive-syncsketch-event",
             description="Create an Ayon Event, from a SyncSketch event.",
@@ -48,45 +58,60 @@ class SyncsketchAddon(BaseServerAddon):
         create it.
         """
         addon_settings = await self.get_studio_settings()
-        ayon_endpoint = f"{ayonconfig.http_listen_address}/addons/{self.name}/{self.version}/syncsketch-event"
+        addon_settings = addon_settings.syncsketch_server_configs[0]
+
+        # This really doenst work in local
+        ayon_endpoint = f"{ayonconfig.http_listen_address}/api/addons/{self.name}/{self.version}/syncsketch-event"
 
         if not addon_settings:
             logging.error(f"Unable to get Studio Settings: {self.name} addon.")
             return
 
+        if not all((addon_settings.url, addon_settings.auth_token, addon_settings.auth_user, addon_settings.account_id)):
+            logging.error("Missing data in the Addon settings.")
+            return
 
-        syncsketch_webhooks_endpoint = f"{addon_settings.syncsketch_url}/api/v2/notifications/{addon_settings.account_id}/webhooks/"
+        sk_endpoint = f"{addon_settings.url}/api/v2/notifications/{addon_settings.account_id}/webhooks/"
 
-        payload={}
         headers = {
-            'Authorization': 'apikey {{addon_settings.username}}:{{addon_settings.api_key}}',
-            'Content-Type': 'application/json'
+            "Authorization": f"apikey {addon_settings.auth_user}:{addon_settings.auth_token}",
+            "Content-Type": "application/json",
         }
 
-        existing_webhooks = requests.request("GET", syncsketch_webhooks_endpoint, headers=headers, data=payload)
-        if response.text:
-            print(response.text)
+        existing_webhooks = requests.request("GET", sk_endpoint, headers=headers)
+
+        if existing_webhooks.status_code == 400:
+            logging.error(existing_webhooks.json())
+            return
+
+        if existing_webhooks.text:
+            # Check if the webhook we care already exists and return early
+            print(existing_webhooks.text)
+            print(existing_webhooks.json())
             pass
-        else:
-            # Create the Webhook
-            payload = json.dumps({
+
+        # Create the Webhook that sends an event when a session ends
+        webhook_created = requests.request(
+            "POST",
+            sk_endpoint,
+            headers=headers,
+            data={
                 "url": ayon_endpoint,
-                "type": "item_created",
-                "headers": {
-                    "X-My-Custom-Header": "some-auth-token-etc" # Grab Ayon Auth token
-                }
-            })
+                "type": "review_ended",
+            },
+        )
 
-            response = requests.request("POST", url, headers=headers, data=payload)
+        if webhook_created.status_code != 200:
+            logging.info("Something went wrong when trying to create the Webhook.")
+            raise ValueError(webhook_created.json())
 
-            print(response.text)
+        logging.info(
+            f"Succesfully created a Webhook in SyncSketch {webhook_created.json()}"
+        )
 
-
-    async def _syncsketch_event(self, syncsketch_payload) -> Dict:
-        """ Whenever SyncSket sends an event to the defined webhook, dispatch an
-            Ayon event.
+    async def _syncsketch_event(self, request: SyncsketchRequestModel):
+        """Dispatch an Ayon event from a SyncSketch one.
         """
-        print("syncsketch_payload", syncsketch_payload)
         print("syncsketch locals", locals())
         # event_id = await dispatch_event(
         #     "syncsketch.event",
@@ -117,7 +142,7 @@ class SyncsketchAddon(BaseServerAddon):
         id_attribute_data = {
             "type": "string",
             "title": "SyncSketch ID",
-            "inherit": False
+            "inherit": False,
         }
 
         id_scope = ["project", "version"]
@@ -131,23 +156,25 @@ class SyncsketchAddon(BaseServerAddon):
             position += 1
             if row["name"] == id_attrib_name:
                 # Check if scope is matching ftrack addon requirements
-                if (set(row["scope"]) == set(id_scope)):
+                if set(row["scope"]) == set(id_scope):
                     id_matches = True
                 id_match_position = row["position"]
 
         if id_matches:
             return False
 
-        postgres_query = "\n".join((
-            "INSERT INTO public.attributes",
-            "    (name, position, scope, data)",
-            "VALUES",
-            "    ($1, $2, $3, $4)",
-            "ON CONFLICT (name)",
-            "DO UPDATE SET",
-            "    scope = $3,",
-            "    data = $4",
-        ))
+        postgres_query = "\n".join(
+            (
+                "INSERT INTO public.attributes",
+                "    (name, position, scope, data)",
+                "VALUES",
+                "    ($1, $2, $3, $4)",
+                "ON CONFLICT (name)",
+                "DO UPDATE SET",
+                "    scope = $3,",
+                "    data = $4",
+            )
+        )
         if not id_matches:
             # Reuse position from found attribute
             if id_match_position is None:
