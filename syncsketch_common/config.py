f13fde5bc056b0535bb7159ac4dee23cbde1e0ea
Jakub Jezek <jakubjezek001@gmail.com>
2023-08-18 15:04:34 +0200

    adding secret resolver to common config

--- syncsketch_common/config.py
+++ syncsketch_common/config.py
@@ -1,6 +1,5 @@
 
-from openpype.settings import get_project_settings
-from openpype.pipeline import get_current_project_name
+import ayon_api
 from ayon_api import get_addon_project_settings
 
 
@@ -14,6 +13,8 @@ def get_syncsketch_project_config(project_name, addon_version):
     Returns:
         dict: SyncSketch config
     """
+    from openpype.settings import get_project_settings
+    from openpype.pipeline import get_current_project_name
 
     project_settings = get_addon_project_settings(
         "syncsketch",
@@ -28,3 +29,27 @@ def get_syncsketch_project_config(project_name, addon_version):
         )
 
     return project_settings["syncsketch_server_config"]
+
+
+def get_resolved_secrets(syncsk_server_config):
+    """ Get resolved secrets from the server config.
+
+    Args:
+        syncsk_server_config (dict): The server config dict.
+
+    Returns:
+        dict: The resolved secrets.
+    """
+    from .constants import required_secret_keys
+
+    all_secrets = ayon_api.get_secrets()
+    secrets = {secret["name"]: secret["value"] for secret in all_secrets}
+
+    # resolve all secrets from the server config
+    resolved_secrets = {
+        key_: secrets[syncsk_server_config[key_]]
+        for key_ in required_secret_keys
+        if syncsk_server_config[key_] in secrets
+    }
+
+    return resolved_secrets
