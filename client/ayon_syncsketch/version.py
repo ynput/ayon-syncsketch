2c37e52d5b7129bcf1a36d1810281ea3ee7ae10f
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-10 15:12:02 +0200

    lowered version and push client version

---
+++ client/ayon_syncsketch/version.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Package declaring AYON addon 'syncsketch' version."""
+__version__ = "0.1.2-dev.1"
--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 """Package declaring addon version."""
 name = "syncsketch"
 title = "SyncSketch"
-version = "0.2.0"
+version = "0.1.2-dev.1"
 client_dir = "ayon_syncsketch"
 
 # TODO: need to make sure image is published to docker hub
