acc77473f301cafdef46083c86b89281773d6925
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-07-03 12:02:26 +0200

    added compatibility and requirement

--- package.py
+++ package.py
@@ -8,7 +8,10 @@ services = {
     "processor": {"image": "ynput/ayon-syncsketch-processor:1.0.0"}
 }
 
+ayon_server_version = ">=1.15.9"
 ayon_required_addons = {
     "core": ">=0.4.0",
 }
-ayon_compatible_addons = {}
+ayon_compatible_addons = {
+    "review": ">=0.6.0",
+}
